8bbeed5f922b28b0509b785e6ecfb77518e6186d
Meghana <53141586+lathamithu@users.noreply.github.com>
2020-03-22 02:11:27 +0530

    Add files via upload

---
+++ AJAX/main.js
@@ -0,0 +1,41 @@
+function Do(){
+var xhr  = new XMLHttpRequest();
+var type = 'POST';
+var url  = 'https://api.backendless.com/36DC4E7A-2B33-40D0-A382-FD197E23A89B/BC24AD0C-019C-42A0-BAAD-229B8D8F7222/data/AJAX';
+var data = {
+  "List" : "Walking"
+};
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        console.log(xhr.responseText);
+    }
+};
+
+xhr.open(type, url, true);
+
+xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+
+xhr.send(JSON.stringify(data));
+}
+
+/*
+        
+        var options = {}; 
+        options.data = JSON.stringify({
+            "List" : str});
+          
+          options.type    = 'POST';
+          options.url     = 'https://api.backendless.com/36DC4E7A-2B33-40D0-A382-FD197E23A89B/BC24AD0C-019C-42A0-BAAD-229B8D8F7222/data/AJAX';
+          
+          options.success = function(resp, status, xhr) {
+              console.log(resp, status, xhr);
+          };
+          
+          options.error = function(xhr) {
+              console.log(JSON.parse(xhr.responseText), xhr);
+          };
+          $.ajax(options.url,options);
+        });
+    });
+    */
