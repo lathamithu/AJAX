0d631e3984e277eb1a45a72d556b8f2831824c0d
Meghana <53141586+lathamithu@users.noreply.github.com>
2020-03-22 02:32:59 +0530

    Update main.js

--- AJAX/main.js
+++ AJAX/main.js
@@ -1,5 +1,5 @@
 function Do(){
-var str=document.getElementById("list").value;
+    var str=document.getElementById("list").value;
 var xhr  = new XMLHttpRequest();
 var type = 'POST';
 var url  = 'https://api.backendless.com/36DC4E7A-2B33-40D0-A382-FD197E23A89B/BC24AD0C-019C-42A0-BAAD-229B8D8F7222/data/AJAX';
@@ -18,5 +18,9 @@ xhr.open(type, url, true);
 xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
 
 xhr.send(JSON.stringify(data));
+var ul = document.getElementById("lists");
+var li = document.createElement("li");
+li.appendChild(document.createTextNode(str));
+ul.appendChild(li);
+document.getElementById("list").value = "";
 }
-
