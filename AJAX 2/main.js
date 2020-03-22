1e85c8e3bc65083de21d18fd5fd8f746f2655943
Meghana <53141586+lathamithu@users.noreply.github.com>
2020-03-23 02:49:21 +0530

    Update main.js

--- AJAX 2/main.js
+++ AJAX 2/main.js
@@ -1,3 +1,27 @@
+        $(document).ready(function(){
+            $("#list").click(function(){
+
+                var txt = JSON.stringify( { "List": ($('#list').val()) } ); 
+                $.ajax({ 
+                    url: 'https://api.backendless.com/36DC4E7A-2B33-40D0-A382-FD197E23A89B/BC24AD0C-019C-42A0-BAAD-229B8D8F7222/data/AJAX',
+                    type: 'post',
+                    contentType: 'application/json',
+                    data: txt,
+                    success: function( data){
+                        
+                        $('#ul').html(JSON.stringify( data ) );
+                        alert(data);
+                    },
+                    error: function(errorThrown ){
+                        console.log( errorThrown );
+                    }
+                });
+
+            });
+            
+            
+          });
+
 
 
     $.ajax({ 
